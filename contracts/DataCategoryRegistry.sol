3537fc73d142ba27aa3af168e84895b1396c19ca
ByeongSu Hong <frostornge@gmail.com>
2018-07-27 18:18:56 +0900

    Add Data Category Registry - manage data category

---
+++ contracts/DataCategoryRegistry.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.4.23;
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract DataCategoryRegistry is Ownable{
+    struct Category {
+        string name;
+        string icon;
+        uint256 reputation;
+        bool isValid;
+    }
+
+    mapping (bytes32 => Category) categories;
+
+    event CategoryRegistered(bytes32 id, string name);
+    event CategoryUnregistered(bytes32 id, string name);
+
+    function register(bytes32 id, string name, string icon, uint256 reputation) public onlyOwner {
+        require(!categories[id].isValid, "Category already exists.");
+        categories[id] = Category(name, icon, reputation, true);
+        emit CategoryRegistered(id, name);
+    }
+
+    function unregister(bytes32 id) public onlyOwner {
+        Category memory category = categories[id];
+        delete categories[id];
+        emit CategoryUnregistered(id, category.name);
+    }
+
+    function validate(bytes32 id) public view returns (bool) {
+        return categories[id].isValid;
+    }
+
+    function validateMany(bytes32[] ids) public view returns (bool) {
+        for (uint256 i = 0; i < ids.length; i++) {
+            if(!validate(ids[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
