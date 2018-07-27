c838cb4193595414852c6b3817b58736ddcf0327
ByeongSu Hong <frostornge@gmail.com>
2018-07-27 18:14:32 +0900

    Update App Registry - minimalize functions

--- contracts/AppRegistry.sol
+++ contracts/AppRegistry.sol
@@ -1,8 +1,10 @@
 pragma solidity ^0.4.23;
 
-import "openzeppelin-solidity/contracts/examples/SimpleToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+
 import "./RiskTokenLockRegistry.sol";
 import "./App.sol";
+import "./AirContainer.sol";
 
 /**
  * AppRegistry stores an app information, and user IDs of the app.
@@ -11,12 +13,12 @@ import "./App.sol";
  * If an app did some bad things that prohibited by Airbloc Protocol's Law,
  * then there's a risk for app can LOSE some amount of it's stake.
  */
-contract AppRegistry is RiskTokenLockRegistry {
+contract AppRegistry {
     using AddressUtils for address;
     using SafeMath for uint256;
 
-    address public receiver;
-
+    ERC20 token;
+    AirContainer public receiver;
     mapping(bytes32 => App) public apps;
 
     event AppRegistered(bytes32 appId, address owner);
@@ -27,77 +29,31 @@ contract AppRegistry is RiskTokenLockRegistry {
      * @param penaltyBeneficiary The destination wallet that stake losses are transferred to.
      */
     constructor(
-        ERC20 token,
+        ERC20 _token,
         address penaltyBeneficiary,
         address punisher,
-        address defaultReceiver
-    )
-        RiskTokenLockRegistry(token, penaltyBeneficiary, punisher)
-        public
-    {
-        require(defaultReceiver != address(0));
-        receiver = defaultReceiver;
+        AirContainer _receiver
+    ) public {
+        token = _token;
+        receiver = _receiver;
     }
 
     /**
      * @param appId ID of off-chain app metadata.
      */
     function register(bytes32 appId) public {
-        apps[appId] = new App(token);
+        apps[appId] = new App(token, receiver);
         emit AppRegistered(appId, msg.sender);
     }
 
     function unregister(bytes32 appId) public {
-        require(hasAppOf(appId), "App not found.");
         App app = apps[appId];
-        require(app.owner == msg.sender, "Only app owner can do this.");
+
+        require(hasAppOf(appId), "App not found.");
+        require(app.owner() == msg.sender, "Only app owner can do this.");
 
         delete apps[appId];
-        delete addressOf[appId];
-        withdraw(stakeOf(msg.sender));
         emit AppUnregistered(appId, msg.sender);
-    }
-
-    /**
-     * Add user to app.
-     */
-    function addUsers(bytes32 appId, bytes32[] ids) public {
-        require(hasAppOf(appId), "App not found.");
-        App app = apps[appId];
-        require(app.owner == msg.sender, "Only app owner can do this.");
-        require(
-            stakeOf(msg.sender) >= getRequiredStake(app.userCount + ids.length),
-            "Insufficient stake amount."
-        );
-
-        for (uint256 i = 0; i < ids.length; i++) {
-            app.addUser(ids[i], receiver);
-        }
-    }
-
-    function removeUsers(bytes32 appId, bytes32[] ids)
-        public
-        onlyAppOwner(appId)
-    {
-        require(hasAppOf(appId), "App not found.");
-        App app = apps[appId];
-        require(app.owner == msg.sender, "Only app owner can do this.");
-
-        for (uint256 i = 0; i < ids.length; i++) {
-            app.removeUser(ids[i]);
-        }
-    }
-
-    // @Override TokenLockRegistry
-    function withdraw(bytes32 appId, uint256 amount) public {
-        require(hasAppOf(appId), "App not found.");
-        App app = apps[appId];
-        require(app.owner == msg.sender, "Only app owner can do this.");
-        require(
-            stakeOf(msg.sender) - amount >= getRequiredStake(app.userCount),
-            "Insufficient stake amount."
-        );
-        super.withdraw(amount);
     }
 
     function hasAppOf(bytes32 appId)
@@ -105,14 +61,6 @@ contract AppRegistry is RiskTokenLockRegistry {
         view
         returns (bool)
     {
-        return apps[appId] != bytes32(0);
-    }
-
-    function getRequiredStake(uint256 userCount)
-        public
-        pure
-        returns (uint256)
-    {
-        return userCount;
+        return apps[appId].token() != address(0);
     }
 }
